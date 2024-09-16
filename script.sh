7ee30aed2e2acc854b800ffb6164b65e83926a57
T00n <68474047+taleen98@users.noreply.github.com>
2024-09-16 14:52:22 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-list=$(find ./remote -type d)    ### change directory.
-for i in  $list
-do
-        $(touch $i/t00n 2>/dev/null)
-        if [ $? -eq 0 ]   ### 0 is the standard return code for a successful completion, 1 is the opposite.
-        then
-                tput setaf 6; echo "[+] This Directory Is Writable!:";
-                echo $i
+# List of all directories inside ./remote (change the path as needed)
+list=$(find ./smbshare -type d) ### change directory.
 
-        fi
+for i in $list
+do
+    # Try to create a file in the directory to test writability
+    touch "$i/t00n" 2>/dev/null
+
+    # Check if the previous command (touch) was successful
+    if [ $? -eq 0 ] ### 0 is the standard return code for a successful completion, 1 is the opposite.
+    then
+        # If successful (writable), print a message
+        tput setaf 6; echo "[+] This Directory Is Writable!:"
+        echo "$i"
+
+        # Remove the test file created
+        rm "$i/t00n"
+    else
+        # If not writable, print a different message
+        tput setaf 1; echo "[-] This Directory Is Not Writable:"
+        echo "$i"
+    fi
 done
